f8f11d5002544f0a37bc9e8888ec931529c12b33
SebastiamLiyo <liyodesing@gmail.com>
2026-03-19 11:03:53 -0500

    fix: augment react JSX module for spline-viewer custom element

--- types/spline-viewer.d.ts
+++ types/spline-viewer.d.ts
@@ -1,10 +1,14 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'spline-viewer': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      url?: string;
-    };
+import 'react';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'spline-viewer': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        url?: string;
+      };
+    }
   }
 }
